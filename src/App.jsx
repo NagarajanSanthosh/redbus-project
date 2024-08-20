377cd563cc8266a750a9a410f1733da0003776f3
NagarajanSanthosh <64208ns@gmail.com>
2024-08-20 13:55:46 +0530

    Hello world - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="font-bold text-3xl">Hello World!</h1>
   )
 }
